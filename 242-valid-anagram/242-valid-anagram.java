8606812fc9baa2456ad02e1e95916d5e78684c61
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2022-07-15 16:19:42 +0530

    Time: 30 ms (13.69%), Space: 47 MB (8.77%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s1, String s2) {
+        if(s2.length()!=s1.length())
+return false;
+HashMap<Character,Integer> hmap = new HashMap<>();
+for(int i=0;i<s1.length();i++){
+   if(hmap.containsKey(s1.charAt(i))){  
+   hmap.put(s1.charAt(i),hmap.get(s1.charAt(i))+1);
+    }
+    else{
+    hmap.put(s1.charAt(i),1);
+    }
+}
+for(int i=0;i<s2.length();i++)
+{
+   if(!hmap.containsKey(s2.charAt(i)))
+   return false;
+   else
+   {
+   if(hmap.get(s2.charAt(i))==1)
+   hmap.remove(s2.charAt(i));
+   else
+   hmap.put(s2.charAt(i),hmap.get(s2.charAt(i))-1);
+   }
+}
+   return true;
+    }
+}
