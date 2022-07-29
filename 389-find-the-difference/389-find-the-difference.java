d1e14e35d386db62299d0fd2f563f195ce46def0
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2022-07-29 22:21:14 +0530

    Time: 3 ms (55.20%), Space: 42.3 MB (48.30%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -14,8 +14,7 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             if(arr[i] != 0 )
             {
-                ch = (char)(i + 97);
-                break;
+                return (char)(i + 97);
             }
         }
         
