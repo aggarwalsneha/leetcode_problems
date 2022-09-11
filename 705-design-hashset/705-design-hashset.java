183c9cea6ba7aabd88a844dccb9780a0797e0170
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2022-09-11 15:50:14 +0530

    Time: 41 ms (42.93%), Space: 84.4 MB (15.34%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,54 +1,20 @@
-
 class MyHashSet {
-ListNode head=null;
+int size=(int)Math.pow(10,6)+1;
+    boolean[] flag=new boolean[size];
     public MyHashSet() {
         
     }
     
     public void add(int key) {
-        if(head==null)
-            head=new ListNode(key);
-        else
-        {
-            ListNode node=new ListNode(key);
-            ListNode temp=head;
-            boolean present=false;
-            while(temp.next!=null)
-            {
-                if(temp.val==key){
-                    present=true;
-                    break;
-                }
-                temp=temp.next;
-            }
-            if(!present && temp.val!=key)
-                temp.next=node;
-        }
+        flag[key]=true;
     }
     
     public void remove(int key) {
-        if(head!=null && head.val==key)
-            head=head.next;
-        else
-        {
-            ListNode temp=head;
-            while(temp!=null && temp.next!=null)
-            {   if(temp.next.val==key)
-                    temp.next=temp.next.next;
-                temp=temp.next;
-            }
-        }
+        flag[key]=false;
     }
     
     public boolean contains(int key) {
-        ListNode temp=head;
-        while(temp!=null)
-        {
-            if(temp.val==key)
-                return true;
-            temp=temp.next;
-        }
-        return false;
+        return flag[key];
     }
 }
 
