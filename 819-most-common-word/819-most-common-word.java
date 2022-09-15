3ae4809573998b866411df15c595bc9b3498990a
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2022-09-15 19:55:38 +0530

    Time: 448 ms (5.01%), Space: 45.1 MB (17.76%) - LeetHub

--- 819-most-common-word/819-most-common-word.java
+++ 819-most-common-word/819-most-common-word.java
@@ -6,11 +6,19 @@ class Solution {
         for(String w:banned)
             bannedW.add(w);
         HashMap<String,Integer> hm=new HashMap();
+        int max=0;
+        String ans="";
         for(String w:words)
         {
             if(!bannedW.contains(w))
+            {
                 hm.put(w,hm.getOrDefault(w,0)+1);
+            if (hm.get(w)>=max) {
+                max=hm.get(w);
+                ans=w;
+            }
+            }
         }
-        return Collections.max(hm.entrySet(),Map.Entry.comparingByValue()).getKey();
+        return ans;
     }
 }
