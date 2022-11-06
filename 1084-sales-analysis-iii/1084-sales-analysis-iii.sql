cc895b5786647008b5be1ddb22cc97ef224d09f8
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2022-11-06 18:14:37 +0530

    Time: 1657 ms (55.79%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-SELECT product_id,product_name FROM Product WHERE product_id NOT IN(SELECT product_id FROM Sales WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31' GROUP BY product_id ) AND product_id IN(SELECT product_id FROM Sales);
+
+
+SELECT distinct p.product_id,p.product_name FROM Product p JOIN Sales s ON p.product_id=s.product_id WHERE p.product_id NOT IN(SELECT product_id FROM Sales WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31');
