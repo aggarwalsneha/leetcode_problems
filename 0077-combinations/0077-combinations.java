888684c05685955d8d42f9d6d34f9c6050832fd5
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2022-12-03 19:36:59 +0530

    Time: 53 ms (37.97%), Space: 54.9 MB (41.96%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>> ans=new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> in=new ArrayList<>();
+        getcomb(1,in,n,k);
+        return ans;
+    }
+    
+     public void getcomb(int start,List<Integer> in,int n,int k)
+    {
+        if(in.size()==k)
+            ans.add(new ArrayList<>(in));
+        for(int i=start;i<=n;i++)
+        {
+            in.add(i);
+            getcomb(i+1,in,n,k);
+            in.remove(in.size()-1);
+        }
+         
+     }
+}
