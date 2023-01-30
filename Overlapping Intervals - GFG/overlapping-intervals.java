cf2c95bd812a59549c94dbbba044f6eb9a655e4d
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2023-01-31 00:13:50 +0530

    Added solution - LeetHub

---
+++ Overlapping Intervals - GFG/overlapping-intervals.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] s = br.readLine().trim().split(" ");
+            int[][] Intervals = new int[n][2];
+            int j = 0;
+            for(int i = 0; i < n; i++){
+                Intervals[i][0] = Integer.parseInt(s[j]);
+                j++;
+                Intervals[i][1] = Integer.parseInt(s[j]);
+                j++;
+            }
+            Solution obj = new Solution();
+            int[][] ans = obj.overlappedInterval(Intervals);
+            for(int i = 0; i < ans.length; i++){
+                for(j = 0; j < ans[i].length; j++){
+                    System.out.print(ans[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int[][] overlappedInterval(int[][] intervals)
+    {
+        // Code here // Code here
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        ArrayList<int[]> ans=new ArrayList<>();
+        int[] curr=intervals[0];
+        for(int i=1;i<intervals.length;i++)
+        {
+            if(curr[1]>=intervals[i][0])
+            {
+                curr[1]=Math.max(curr[1],intervals[i][1]);
+            }
+            else
+            {
+                ans.add(curr);
+                curr=intervals[i];
+            }
+        }
+        ans.add(curr);
+        int[][]res=new int[ans.size()][2];
+        return ans.toArray(res);
+    }
+}
