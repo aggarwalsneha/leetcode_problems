1efa40888cd350971f0627489eff69f8f2152b64
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2023-02-11 11:30:53 +0530

    Added solution - LeetHub

---
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class MaxLenZeroSumSub
+{
+
+    // Returns length of the maximum length subarray with 0 sum
+
+    // Drive method
+    public static void main(String arg[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+
+            GfG g = new GfG();
+            System.out.println(g.maxLen(arr, n));
+            T--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class GfG
+{
+    int maxLen(int arr[], int n)
+    {
+        // Your code here
+        int maxLen=0,sum=0;
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        hm.put(0,-1);
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+            if(hm.containsKey(sum))
+            {
+                int len=i-hm.get(sum);
+                maxLen=Math.max(maxLen,len);
+            }
+            else
+            hm.put(sum,i);
+        }
+        return maxLen;
+    }
+}
