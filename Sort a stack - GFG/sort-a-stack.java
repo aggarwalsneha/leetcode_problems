bbfc0497ab3143eee4d6ee8a55e477ef43fd69e3
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2023-02-26 22:52:30 +0530

    Added solution - LeetHub

---
+++ Sort a stack - GFG/sort-a-stack.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.Stack;
+class SortedStack{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Stack<Integer> s=new Stack<>();
+			int n=sc.nextInt();
+			while(n-->0)
+			s.push(sc.nextInt());
+			GfG g=new GfG();
+			Stack<Integer> a=g.sort(s);
+			while(!a.empty()){
+				System.out.print(a.peek()+" ");
+				a.pop();
+			}
+			System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+class GfG{
+	public Stack<Integer> sort(Stack<Integer> s)
+	{
+		//add code here.
+		if(s.isEmpty() || s.size()==1)
+		return s;
+		int x=s.pop();
+		sort(s);
+		insert(s,x);
+		return s;
+	}
+	public void insert(Stack<Integer>s,int x)
+	{
+	    if(s.isEmpty() || s.peek()<=x)
+	    s.push(x);
+	    else{
+	        int top=s.pop();
+	        insert(s,x);
+	        s.push(top);
+	    }
+	}
+}
