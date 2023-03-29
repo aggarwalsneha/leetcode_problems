05f4df0d4115b4e29baaa6c5235693a5372fdfb2
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2023-03-29 16:03:24 +0530

    Time: 7 ms (36.34%), Space: 41 MB (49.48%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -2,16 +2,18 @@ class Solution {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
         int n=satisfaction.length;
-        int [][]dp=new int[n+1][n+1];
+        int[]curr=new int[n+1];
+        int[]next=new int[n+1];
         for(int ind=n-1;ind>=0;ind--)
         {
-            for(int time=ind;time>=0;time--)
+            for(int time=0;time<n;time++)
             {
-                int pick=satisfaction[ind]*(time+1)+dp[ind+1][time+1];
-                int nonpick=dp[ind+1][time];
-                dp[ind][time]=Math.max(pick,nonpick);
+                int pick=satisfaction[ind]*(time+1)+next[time+1];
+                int nonpick=next[time];
+                curr[time]=Math.max(pick,nonpick);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 }
