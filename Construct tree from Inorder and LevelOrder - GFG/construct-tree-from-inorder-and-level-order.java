b9f74f1e45214bcbba6daeb3419506e2368c4c31
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2023-03-30 11:32:42 +0530

    Added solution - LeetHub

---
+++ Construct tree from Inorder and LevelOrder - GFG/construct-tree-from-inorder-and-level-order.java
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left, right;
+    
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }    
+        public void setLeft(Node left) 
+    {
+        this.left = left;
+    }
+  
+    public void setRight(Node right) 
+    {
+        this.right = right;
+    }
+        
+    
+}
+
+
+class ConstructBT
+{
+    Node root;
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    
+	    while(t-- > 0)
+	    {
+	        int n = sc.nextInt();
+	        
+	        int inord[] = new int[n];
+	        int level[] = new int[n];
+	        for(int i = 0; i < n; i++)
+	            inord[i] = sc.nextInt();
+	            
+	        for(int i = 0; i < n; i++)
+	             level[i] = sc.nextInt();
+	             
+	        GfG g = new GfG();
+	        Node node =  g.buildTree(inord, level);
+	        printPreOrder(node);
+	        System.out.println();
+	        
+	        
+	    }
+    }
+    
+   static void printPreOrder(Node node)
+   {
+       if(node == null)
+          return;
+          
+        System.out.print(node.data + " ");
+        printPreOrder(node.left);
+        printPreOrder(node.right);
+   }
+}
+
+// } Driver Code Ends
+
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*Complete the function below*/
+class GfG
+{
+    Node buildTree(int inord[], int level[])
+    {
+        //your code here
+        Map<Integer,Integer>hm=new HashMap<>();
+        for(int i=0;i<level.length;i++){
+        hm.put(inord[i],i);
+        }
+        return build(level,0,0,level.length-1,hm);
+    }
+    
+    Node build(int[]level,int l,int is,int ie,Map<Integer,Integer>inorder)
+    {
+        if(is>ie)
+        return null;
+        int ind=inorder.get(level[l]);
+        Node root=new Node(level[l]);
+        root.left=build(level,2*l+1,is,ind-1,inorder);
+        root.right=build(level,2*l+2,ind+1,ie,inorder);
+        return root;
+    }
+    
+   
+}
+
+
