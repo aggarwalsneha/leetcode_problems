ed51bd7b247ae50843e8f592b394bcce0c618051
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2023-04-04 18:03:47 +0530

    Added solution - LeetHub

---
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
+        // Code here
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+        adj.add(new ArrayList<>());
+        }
+        for(int[] e:edges)
+        {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        int[] dist=new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        Queue<Integer>q=new LinkedList<>();
+        q.offer(src);
+        dist[src]=0;
+        while(!q.isEmpty())
+        {
+            int node=q.poll();
+            for(int num:adj.get(node)){
+                if(dist[node]+1<dist[num]){
+                dist[num]=dist[node]+1;
+                q.offer(num);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(dist[i]==Integer.MAX_VALUE){
+            dist[i]=-1;
+            }
+        }
+        return dist;
+    }
+}
