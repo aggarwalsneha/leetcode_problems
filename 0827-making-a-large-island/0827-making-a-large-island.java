2888c188ce2709d3871c5177d5c4638b76a9db0f
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2023-04-10 16:06:37 +0530

    Time: 225 ms (25.51%), Space: 81.9 MB (35.45%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -0,0 +1,83 @@
+class Solution {
+    public int largestIsland(int[][] grid) {
+        int n=grid.length;
+        DisjointSet ds=new DisjointSet(n*n);
+        int[] x=new int[]{0,0,1,-1};
+        int[] y=new int[]{1,-1,0,0};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    for(int k=0;k<4;k++){
+                        int new_r=i+x[k];
+                        int new_c=j+y[k];
+                        if(new_r>=0 && new_r<n && new_c<n && new_c>=0 && grid[new_r][new_c]==1){
+                            int node=i*n+j;
+                            int adj=new_r*n+new_c;
+                            ds.unionBySize(node,adj);
+                        }
+                    }
+                }
+            }
+        }
+        int max=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    Set<Integer> Compo=new HashSet<>();
+                    for(int k=0;k<4;k++){
+                        int new_r=i+x[k];
+                        int new_c=j+y[k];
+                        if(new_r>=0 && new_r<n && new_c<n && new_c>=0 && grid[new_r][new_c]==1){
+                            int adj=new_r*n+new_c;
+                            Compo.add(ds.findpar(adj));
+                        }
+                    }
+                    int size=0;
+                    for(int el:Compo){
+                        size+=ds.size.get(el);
+                    }
+                    max=Math.max(max,size+1);
+                }
+            }
+        }
+        for(int i=0;i<n*n;i++){
+            max=Math.max(max,ds.size.get(ds.findpar(i)));
+        }
+        return max;
+    }
+}
+
+class DisjointSet{
+    List<Integer>parent=new ArrayList<>();
+    List<Integer>size=new ArrayList<>();
+    
+    DisjointSet(int n){
+        for(int i=0;i<n;i++){
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    
+    int findpar(int node){
+        if(node==parent.get(node))
+        return node;
+        int ulp=findpar(parent.get(node));
+        parent.set(node,ulp);
+        return parent.get(node);
+    }
+    
+    void unionBySize(int u,int v){
+        int up=findpar(u);
+        int vp=findpar(v);
+        if(up==vp)
+        return;
+        if(size.get(up)<size.get(vp)){
+            parent.set(up,vp);
+            size.set(vp,size.get(up)+size.get(vp));
+        }
+        else{
+            parent.set(vp,up);
+            size.set(up,size.get(up)+size.get(vp));
+        }
+    }
+}
