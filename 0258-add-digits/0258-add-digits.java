4e9931656c90fdf0f712c7f11e0b4aeac6c82a5d
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2023-04-26 11:23:06 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (97.65%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+      int digit = 0;
+        while (num>0) {
+            digit += num % 10;
+            num=num/10;
+            
+            if (num==0 && digit>9) {
+                num=digit;
+                digit=0;  
+            }    
+        }     
+        return digit;
+        
+    }
+}
